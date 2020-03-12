e7e459225acb3d1bc5ca793f055644e7f813c467
despaink <43147974+despaink@users.noreply.github.com>
2020-03-12 10:32:10 -0600

    removed unneeded data

--- src/views/login.vue
+++ src/views/login.vue
@@ -20,45 +20,7 @@ import { AmplifyEventBus, components } from "aws-amplify-vue"
                 snackbar: false,
                 message : "Hello there",
                 store : dataStore,
-                authConfig: {
-                    signUpConfig: {
-                        header: 'My Customized Sign Up',
-                        hideAllDefaults: true,
-                        defaultCountryCode: '1',
-                        signUpFields: [
-                        {
-                            label: 'Email',
-                            key: 'email',
-                            required: true,
-                            displayOrder: 1,
-                            type: 'string',
-                            signUpWith: true
-                        },
-                        {
-                            label: 'Password',
-                            key: 'password',
-                            required: true,
-                            displayOrder: 2,
-                            type: 'password'
-                        },
-                        {
-                            label: 'PhoneNumber',
-                            key: 'phone_number',
-                            required: true,
-                            displayOrder: 3,
-                            type: 'string'
-                        },
-                        {
-                            label: 'Custom Attribute',
-                            key: 'custom_attr',
-                            required: false,
-                            displayOrder: 4,
-                            type: 'string',
-                            custom: true
-                        }
-                        ]
-                    }
-                    },
+                
         }),
         mounted() {
             AmplifyEventBus.$on("authState", eventInfo => {
