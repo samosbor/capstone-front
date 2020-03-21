5a6db738f9d9e934fd3565fb371fcaadf0c35dbe
samosbor <samosbor@byu.edu>
2020-03-20 18:06:55 -0600

    Campains shows 2 graphs

--- src/views/Campaigns.vue
+++ src/views/Campaigns.vue
@@ -14,12 +14,21 @@
         </v-card>
       </v-col>
       <v-col cols="9">
-        <v-card v-if="dataTable.length > 0" class="pa-5">
+        <v-card v-if="dataTable.length > 0" class="pa-5 my-5">
           <GChart
-            :settings="{packages: ['bar']}"
+            :settings="{packages: ['corechart']}"
             :data="dataTable"
-            :options="chartOptions"
-            :createChart="(el, google) => new google.charts.Bar(el)"
+
+            :createChart="(el, google) => new google.visualization.ColumnChart(el)"
+            @ready="onChartReady"
+            width="90%"
+          />
+        </v-card>
+        <v-card v-if="data2.length > 0" class="pa-5 my-5">
+          <GChart
+            :settings="{packages: ['corechart']}"
+            :data="data2"
+            :createChart="(el, google) => new google.visualization.ColumnChart(el)"
             @ready="onChartReady"
             width="90%"
           />
@@ -33,34 +42,39 @@
 import exportService from "@/misc/exportService.js";
 import { dataStore } from "@/misc/dataStore";
 import createCampaign from "@/components/createCampaign.vue";
+import { GChart } from 'vue-google-charts'
 export default {
   name: "Campaigns",
   components: {
-    createCampaign
+    createCampaign,
+    GChart
   },
   data: () => ({
+    dataStore,
     storeName: dataStore.currentStoreName,
     campaigns: [],
     selectedCampaign: "",
     dataTable: [],
     chartsLib: null,
+    data1: [],
+    data2: []
   }),
   computed: {},
   created() {
     this.getCampaigns();
   },
   computed: {
-    chartOptions () {
-      if (!this.chartsLib) return null
+    chartOptions() {
+      if (!this.chartsLib) return null;
       return this.chartsLib.charts.Bar.convertOptions({
         chart: {
-          title: this.selectedQuery.chartTitle,
+          title: this.selectedCampaign,
           subtitle: this.date
         },
-        hAxis: { format: 'decimal' },
-        colors: ['#1b9e77', '#d95f02', '#7570b3']
-      })
-    },
+        hAxis: { format: "decimal" },
+        colors: ["#1b9e77", "#d95f02", "#7570b3"]
+      });
+    }
   },
   methods: {
     setCurrentDate() {
@@ -101,6 +115,51 @@ export default {
     },
     setCampaign(campaign) {
       this.selectedCampaign = campaign;
+      this.getChartData();
+    },
+    onChartReady(chart, google) {
+      this.chartsLib = google;
+    },
+    async getChartData() {
+      let base = "https://s3.us-east-2.amazonaws.com/jolt.capstone/";
+      let prefix =
+        "athena-query-logs/" +
+        this.dataStore.currentStoreName +
+        "/campaigns/" +
+        this.selectedCampaign +
+        "/";
+      let data = await this.axios
+        .get(base + "?prefix=" + prefix)
+        .then(response => {
+          var parser = new DOMParser();
+          let data = parser.parseFromString(response.data, "text/xml");
+          return data;
+        });
+      let key1 = data.getElementsByTagName("Key")[0].innerHTML;
+      let key2 = data.getElementsByTagName("Key")[2].innerHTML;
+
+      let data1 = await this.$papa.parse(base + key1, {
+        download: true,
+        dynamicTyping: true,
+        complete: (results) => {
+          results.data.pop();
+          results.data.pop();
+          this.data1 = results.data
+          this.dataTable = results.data
+          return results.data;
+        }
+      });
+      console.log("starting next one");
+      let data2 = await this.$papa.parse(base + key2, {
+        download: true,
+        dynamicTyping: true,
+        complete: results => {
+          results.data.pop();
+          results.data.pop();
+          this.data2 = results.data
+          return results.data;
+        }
+      });
     }
   }
 };
